2dff9a1c99d1348a59d7259c3e7a776fa547ea8c
Aleksandr <Sashkakonowawka4@gmail.com>
2021-06-23 00:00:17 +0300

    models products, Company added

--- games/models.py
+++ games/models.py
@@ -15,3 +15,4 @@ class Product(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     price = models.IntegerField()
+    
